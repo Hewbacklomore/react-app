68b49a838f280053b7a4d698c415d1c2295c06ee
Artur Peiev <fromtheisland13@gmail.com>
2023-08-30 12:43:38 +0100

    second_tast

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Card from './components/Card';
+import App from './App';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
-    <Card/>
+    <App /> 
 );
 
 
