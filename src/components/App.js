4c6c0a0b442ca4ca175a95e438b13dab4575d553
Artur Peiev <fromtheisland13@gmail.com>
2023-08-30 13:19:24 +0100

    changed

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from './Card';
+
+function App() {
+  return (
+    <div className = "App">
+      <Card title = "Title 1" />
+      <Card text = "Text 2" />
+      <Card title = "Title 3" text = "Text 3" />
+    </div>
+  );
+}
+
+export default App;
