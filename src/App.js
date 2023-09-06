0ca4638d4bc07ac2386ccebf536a21664d455418
Artur Peiev <fromtheisland13@gmail.com>
2023-09-06 15:40:14 +0100

    task3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import Definitions from './components/Definitions';
+import Progress from './components/Progress';
 
 function App() {
 
-  const definitionsList = [
-    { dt: 'one', dd: 'two', id: 1 },
-    { dt: 'another term', dd: 'another description', id: 2 },
-];
+  
   return (
     <div className='App'>
-      <Definitions data={definitionsList} />
+      <Progress percentage = {40}/>
     </div>
   );
 }
